95107359901aa86800448ca7c641cb8f3ced9df3
Talos-Edurobots <talos.edurobots@gmail.com>
2025-10-10 19:57:52 +0300

    Update pathing constants and add ExampleTeleOp
    Adjusted FollowerConstants and MecanumConstants values in Constants.java for improved tuning. Added ExampleTeleOp.java as a sample teleop mode demonstrating manual and automated path following with slow mode and telemetry integration.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
@@ -14,8 +16,12 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .forwardZeroPowerAcceleration(-33.1000)
-            .lateralZeroPowerAcceleration(-50) // to check
+            .forwardZeroPowerAcceleration(-30.1)
+            .lateralZeroPowerAcceleration(-53) // to check
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.04, 0, 0.001, 0.02))
+            .headingPIDFCoefficients(new PIDFCoefficients(0.8, 0, 0.04, 0.02))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.009, 0, 0, 0.6, 0.01))
+            .centripetalScaling(0.005)
             .mass(7.3);
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
@@ -27,8 +33,8 @@ public class Constants {
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .xVelocity(66.7587)
-            .yVelocity(56.84);
+            .xVelocity(70.4635)
+            .yVelocity(59.7103);
     public static PinpointConstants localizerConstants = new PinpointConstants()
             .forwardPodY(3.4) // check
             .strafePodX(4) // check
