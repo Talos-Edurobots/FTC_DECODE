aba4ed7af26a5c736ede042d12c49f2ba9371b8e
Talos-Edurobots <talos.edurobots@gmail.com>
2025-10-27 19:52:13 +0200

    Add IntakeTestWithDriveTrain opmode and update imports
    Introduces a new TeleOp mode, IntakeTestWithDriveTrain, which combines intake and drivetrain control with IMU-based field-centric driving. Also updates IntakeTest.java to include additional imports for IMU and navigation support.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/IntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/IntakeTest.java
@@ -1,8 +1,12 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp(name = "test intake", group = "testing")
 public class IntakeTest extends LinearOpMode {
