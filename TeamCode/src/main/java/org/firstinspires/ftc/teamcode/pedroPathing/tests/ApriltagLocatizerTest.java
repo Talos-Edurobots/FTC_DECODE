b0a3fe7f35f81027c329d053d4d4b0daa8a5711a
Talos-Edurobots <talos.edurobots@gmail.com>
2025-11-03 19:03:45 +0200

    Add ApriltagLocatizerTest TeleOp for AprilTag localization
    Introduces a new TeleOp mode, ApriltagLocatizerTest, which uses the Limelight3A camera to test AprilTag localization and outputs the robot's position to telemetry.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/tests/ApriltagLocatizerTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.tests;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+@TeleOp(name = "apriltag locatizer test", group = "tests")
+public class ApriltagLocatizerTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Limelight3A limelight;
+
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(1); // april tag localization pipeline
+        waitForStart();
+        limelight.start(); // start
+        while (opModeIsActive()) {
+            LLResult llResult = limelight.getLatestResult();
+            telemetry.addData("tag", llResult.getBotpose().getPosition());
+            telemetry.update();
+        }
+    }
+}
