05ed8b4a5102142414fd5ccddd2a865348d013ab
ManosFps1pt <manos.dragasakis@gmail.com>
2025-11-22 21:16:32 +0200

    Refactor Debugger for generic motor power testing
    Replaced IntakePowerTest with a generic MotorPowerTest class to allow testing of multiple motors without encoders. Updated Debugger to add selectable tests for various motors. Also updated TestBug to use PanelsTelemetry for telemetry output and added a runtime-based cosine calculation.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/main/Debugger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/main/Debugger.java
@@ -1,57 +1,65 @@
 package org.firstinspires.ftc.teamcode.pedroPathing.main;
 
-import com.pedropathing.telemetry.SelectScope;
 import com.pedropathing.telemetry.SelectableOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-import java.util.function.Consumer;
-import java.util.function.Supplier;
-
 @TeleOp(name = "Debugger", group = "main")
 public class Debugger extends SelectableOpMode {
+
     public Debugger() {
         super("Select a Tuning OpMode", s -> {
             s.folder("Without encoder", l -> {
-                l.add("Run Intake", IntakePowerTest::new);
+                l.add("Run Intake", () -> new MotorPowerTest(RobotConstants.INTAKE_NAME));
+                l.add("Run Shooter", () -> new MotorPowerTest(RobotConstants.SHOOTER_NAME));
+                l.add("Run Left Front Drive", () -> new MotorPowerTest(RobotConstants.LEFT_FRONT_NAME));
+                l.add("Run Right Front Drive", () -> new MotorPowerTest(RobotConstants.RIGHT_FRONT_NAME));
+                l.add("Run Left Back Drive", () -> new MotorPowerTest(RobotConstants.LEFT_BACK_NAME));
+                l.add("Run Right Back Drive", () -> new MotorPowerTest(RobotConstants.RIGHT_BACK_NAME));
             });
-
         });
-    };
-    @Override
-    public void onSelect(){}
-}
-
-class IntakePowerTest extends OpMode {
-    DcMotorEx intake;
-    double maxVelocity = 0;
-    @Override
-    public void init_loop() {
-        double maxVelocity = 0;
-        DcMotorEx intake = (DcMotorEx) hardwareMap.dcMotor.get(RobotConstants.INTAKE_NAME);
-        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        intake.setDirection(DcMotor.Direction.REVERSE);
-        telemetry.addLine("Init Complete");
     }
 
     @Override
-    public void init() {
+    public void onSelect() {}
+}
 
+// ===================================================
+// MOTOR POWER TEST
+// ===================================================
+class MotorPowerTest extends OpMode {
+    public MotorPowerTest(String motorName) {
+        this.motorName = motorName;
+    }
+    private final String motorName;
+    private DcMotorEx intake;
+
+    private double maxVelocity = 0;
+
+    @Override
+    public void init() {
+        intake = hardwareMap.get(DcMotorEx.class, motorName);
+        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        intake.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addLine("Init Complete");
+        telemetry.update();
     }
 
     @Override
     public void loop() {
-        intake.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
+        // Set power via triggers
+        double power = gamepad1.right_trigger - gamepad1.left_trigger;
+        intake.setPower(power);
+
         double currentVelocity = intake.getVelocity();
-        telemetry.addData("Intake Power", intake.getPower());
-        telemetry.addData("Intake Velocity", currentVelocity);
-        telemetry.addData("max Velocity", maxVelocity);
         maxVelocity = Math.max(maxVelocity, currentVelocity);
+
+        telemetry.addData("Intake Power", power);
+        telemetry.addData("Intake Velocity", currentVelocity);
+        telemetry.addData("Max Velocity", maxVelocity);
         telemetry.update();
     }
 }
-
-
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/tests/TestBug.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/tests/TestBug.java
@@ -1,12 +1,28 @@
 package org.firstinspires.ftc.teamcode.pedroPathing.tests;
 
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name = "bug test", group = "testing")
-public class TestBug extends LinearOpMode {
-    @Override
-    public void runOpMode() throws InterruptedException {
 
+//todo check if PanelsTelemetry is working on the driver station
+
+@TeleOp(name = "panelsTelemetry", group = "testing")
+public class TestBug extends LinearOpMode {
+    TelemetryManager pTel = PanelsTelemetry.INSTANCE.getTelemetry();
+    @Override
+    public void runOpMode() {
+        double lastTime = getRuntime();
+        pTel.addLine("hello world");
+        pTel.update();
+        waitForStart();
+        while (opModeIsActive()) {
+            double dt = getRuntime() - lastTime;
+            double cos = Math.cos(getRuntime());
+            pTel.addData("cosine of t", cos);
+            lastTime = getRuntime();
+            pTel.update(telemetry);
+        }
     }
 }
