d9fde27f0a16f3a1c07bf536bba4c9c21e09e5fb
ManosFps1pt <manos.dragasakis@gmail.com>
2026-05-11 02:12:57 +0300

    Create v2.pp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/main/auto/v2.pp
@@ -0,0 +1,163 @@
+{
+  "startPoint": {
+    "x": 25,
+    "y": 129,
+    "heading": "linear",
+    "startDeg": 90,
+    "endDeg": 180,
+    "locked": false
+  },
+  "lines": [
+    {
+      "id": "line-khhzn8hcthj",
+      "name": "Shoot preload",
+      "endPoint": {
+        "x": 48,
+        "y": 100,
+        "heading": "linear",
+        "startDeg": 233,
+        "endDeg": 180
+      },
+      "controlPoints": [],
+      "color": "#DCAA57",
+      "locked": false,
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mp0an5v8-bmcpgi",
+      "name": "grab 2 with gate",
+      "endPoint": {
+        "x": 16,
+        "y": 63,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 180,
+        "endDeg": 180
+      },
+      "controlPoints": [
+        {
+          "x": 48,
+          "y": 60
+        },
+        {
+          "x": 42,
+          "y": 60
+        }
+      ],
+      "color": "#89687D",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mp0c5si7-kxfrtq",
+      "name": "score 2",
+      "endPoint": {
+        "x": 60,
+        "y": 78,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 180,
+        "endDeg": 180
+      },
+      "controlPoints": [],
+      "color": "#A58B7B",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mp0cqmit-myyc0q",
+      "name": "open gate 1",
+      "endPoint": {
+        "x": 40,
+        "y": 60,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 180,
+        "endDeg": 180
+      },
+      "controlPoints": [
+        {
+          "x": 54,
+          "y": 66
+        }
+      ],
+      "color": "#8C5C8A",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mp0crlmd-vn40bh",
+      "name": "open gate 2",
+      "endPoint": {
+        "x": 12,
+        "y": 60,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 180,
+        "endDeg": 155
+      },
+      "controlPoints": [],
+      "color": "#B58DB5",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mp0cuuoa-jobh54",
+      "name": "Path 6",
+      "endPoint": {
+        "x": 60,
+        "y": 78,
+        "heading": "linear",
+        "reverse": true,
+        "startDeg": 155,
+        "endDeg": 180
+      },
+      "controlPoints": [],
+      "color": "#557856",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    }
+  ],
+  "shapes": [],
+  "sequence": [
+    {
+      "kind": "path",
+      "lineId": "line-khhzn8hcthj"
+    },
+    {
+      "kind": "path",
+      "lineId": "mp0an5v8-bmcpgi"
+    },
+    {
+      "kind": "path",
+      "lineId": "mp0c5si7-kxfrtq"
+    },
+    {
+      "kind": "path",
+      "lineId": "mp0cqmit-myyc0q"
+    },
+    {
+      "kind": "path",
+      "lineId": "mp0crlmd-vn40bh"
+    },
+    {
+      "kind": "path",
+      "lineId": "mp0cuuoa-jobh54"
+    }
+  ],
+  "version": "1.2.1",
+  "timestamp": "2026-05-10T23:12:33.068Z"
+}
